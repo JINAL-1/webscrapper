c2c89f88f2b8e93e43e5d45c37c9717a676c084e
JINAL-1 <59589615+JINAL-1@users.noreply.github.com>
2021-04-07 15:11:48 -0700

    adding function

--- controllers/index.js
+++ controllers/index.js
@@ -7,20 +7,41 @@ var got = require('got');
 //instead of request-promise OR reset-promise-native
 
 const jsdom = require("jsdom");
+const { html } = require('cheerio');
 const {JSDOM}=jsdom;
 //useful for testing and scraping real-world web applications.
 
 //https://www.google.com/search?q=easter
 
 function day(term){
-  let data={
+  let dom={
     uri:"https://www.google.com/search?q="+term,
-    headers: {
+    headers:
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
-      //displaying as a browser search so that ip address is not blocked
-    }
-  };
+      //displaying as a browser search so that ip address is not blocked,
+      
+      };
+
+      console.log(dom.uri);
+
+      (async () => {
+        try {
+          let {document}=(new JSDOM(html)).window;
+         const day=document.querySelector('.zCubwf').textContent;
+          const response = await got(dom.uri);
+          
+      
+          console.log("");
+          //=> '<!doctype html> ...'
+        } catch (error) {
+          console.log("error.response.body");
+          //=> 'Internal server error ...'
+        }
+      })();
 }
+
+day("Christmas");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
