09300dac6b30140a38b29c4a1ea1c6450452f54e
JINAL-1 <59589615+JINAL-1@users.noreply.github.com>
2021-04-06 23:49:00 -0700

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var cheerio = require('cheerio');
+var got = require('got');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
