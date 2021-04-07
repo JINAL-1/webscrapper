7af1175754b902d0e6517db59a2b88753d703154
JINAL-1 <59589615+JINAL-1@users.noreply.github.com>
2021-04-07 00:07:54 -0700

    commit

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 var cheerio = require('cheerio');
 var got = require('got');
+//instead of request-promise 
+
+const jsdom = require("jsdom");
+//useful for testing and scraping real-world web applications.
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
